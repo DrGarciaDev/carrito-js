bb54c805d205d183b3651aace5756a70b5a476da
Luis García <luis_grdz@hotmail.com>
2020-06-28 18:10:58 -0500

    Video 72: imprimiendo los cursos del localstorage al cargar la pagina

--- js/app.js
+++ js/app.js
@@ -16,6 +16,9 @@ function cargarEventListeners() {
 
     // al vaciar el carrito 
     vaciarCarritoBtn.addEventListener('click', vaciarCarrito);
+
+    // al cargar el documento mostrar el local storage 
+    document.addEventListener('DOMContentLoaded', leerLocalStorage);
 }
 
 
@@ -119,4 +122,32 @@ function obtenerCursosLocalStorage() {
     }
 
     return cursosLS;
+}
+
+// imprime los cursos del locarl storage en el carrito 
+function leerLocalStorage() {
+    let cursosLS;
+
+    cursosLS = obtenerCursosLocalStorage();
+
+    cursosLS.forEach(function(curso){
+        const row = document.createElement('tr');
+
+        row.innerHTML = `
+            <td>
+                <img src="${curso.imagen}" width="100">
+            </td>
+            <td>
+                ${curso.titulo}
+            </td>
+            <td>
+                ${curso.precio}
+            </td>
+            <td>
+                <a href="#" class="borrar-curso"  data-id="${curso.id}">X</a>
+            </td>
+        `;
+    
+        listaCursos.appendChild(row);    
+    });
 }
